22298d81201a106623cf315c9847c5ede3b1e2b1
Resaw <resavv@mail.ru>
2022-07-04 23:28:11 +0300

    updating the application infrastructure

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/app/app";
 
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
